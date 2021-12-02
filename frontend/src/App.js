4d23448018e246e502125d18dc166a8dbc4fd940
Hieu Pham <intelz.hieu@gmail.com>
2021-12-03 00:38:10 +0700

    Add left side Drawer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,46 @@
 import React from "react";
+import { useState } from "react";
 import {
-  AppBar,
   Toolbar,
   IconButton,
   Typography,
   Button,
   InputBase,
 } from "@mui/material";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import MuiAppBar from "@mui/material/AppBar";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
 import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import SearchIcon from "@mui/icons-material/Search";
-import { styled, alpha } from "@mui/material/styles";
+import { styled, alpha, useTheme } from "@mui/material/styles";
+
 import "./App.css";
 
+const drawerWidth = 240;
+
+const customeTheme = createTheme({
+  palette: {
+    warning: {
+      // This is green.A700 as hex.
+      main: "#11cb5f",
+    },
+    hieu12: {
+      // This is green.A700 as hex.
+      main: "#fd4faf",
+    },
+  },
+});
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
   borderRadius: theme.shape.borderRadius,
@@ -52,23 +81,80 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
+// Persistent Drawer
+const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create("margin", {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  })
+);
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-end",
+}));
+
 function App() {
+  const theme = useTheme();
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setIsDrawerOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setIsDrawerOpen(false);
+  };
   return (
-    <>
-      <AppBar position="static">
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed">
         <Toolbar>
           <IconButton
             size="large"
             color="inherit"
             aria-label="menu"
-            sx={{ mr: 2 }}
+            sx={{...(isDrawerOpen && { display: "none" }) }}
+            onClick={handleDrawerOpen}
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" sx={{ flexGrow: 1 }} component="div">
             Dashboard
           </Typography>
-          <InputBase color="info" placeholder="Search..." />
+
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
@@ -81,8 +167,60 @@ function App() {
           <Button color="inherit">LOGIN</Button>
         </Toolbar>
       </AppBar>
-      <h1>Admin Dashboard</h1>
-    </>
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={isDrawerOpen}
+      >
+        <DrawerHeader>
+          <h4>Drawer Header</h4>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List sx={{ backgroundColor: "#1976d2", color: "white" }}>
+          {["Dashboard", "Employee", "Department", "Others"].map(
+            (text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon
+                  sx={{ backgroundColor: "#1976d2", color: "white" }}
+                >
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            )
+          )}
+        </List>
+      </Drawer>
+      <Main open={isDrawerOpen}>
+        <DrawerHeader />
+        <Typography paragraph>
+          Dolore sunt exercitation sint exercitation cupidatat velit. Pariatur
+          nisi excepteur aliquip aute cupidatat anim ad ex anim ipsum. Nostrud
+          aliquip veniam excepteur mollit reprehenderit.
+        </Typography>
+        <h1 style={{ color: "blue" }}>Admin Dashboard</h1>
+        <ThemeProvider theme={customeTheme}>
+          <Button color="warning">Warning</Button>
+          <Button color="primary">Primary</Button>
+          <Button color="hieu12">hieu12</Button>
+        </ThemeProvider>
+      </Main>
+    </Box>
   );
 }
 
