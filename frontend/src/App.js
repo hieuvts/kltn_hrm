261139e88f812f992116f901b7f251ad44d5bfca
Hieu Pham <intelz.hieu@gmail.com>
2021-12-06 00:21:41 +0700

    Update folder structure and package dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { useState } from "react";
 import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import {
   Toolbar,
   IconButton,
   Typography,
