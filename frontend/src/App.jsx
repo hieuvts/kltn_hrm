0079fe67b0a8e28315c7b808b3a00df7ddc85965
Hieu Pham <intelz.hieu@gmail.com>
2021-12-12 22:10:34 +0700

    Removed unused component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,5 @@
 import MyDrawer from "./components/MyDrawer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CustomRoutes from "./components/CustomRoutes";
-import Dashboard from "./pages/Dashboard";
-import Department from "./pages/Department";
-import Employee from "./pages/Dashboard";
-import Others from "./pages/Others";
-import AboutUs from "./pages/AboutUs";
 import NotFound from "./pages/404NotFound";
 import React from "react";
 export default function App() {
