edaa31d1142156a740fad714352a5140cef5b2e6
Hieu Pham <intelz.hieu@gmail.com>
2021-12-13 15:06:15 +0700

    Added 404 Page

--- frontend/src/pages/404NotFound.jsx
+++ frontend/src/pages/404NotFound.jsx
@@ -1,59 +1,30 @@
 import React from "react";
 import { Box, Button, Container, Typography } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import BackgroundImg from "../assets/images/404NotFoundBanner.jpg";
 
+import "./404NotFound.css";
+
+const styles = {
+  paperContainer: {
+    height: "100vh",
+    backgroundImage: `url(${BackgroundImg})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+  },
+};
 export default function NotFound() {
   return (
-    <div>
-      <Box
-        component="main"
-        sx={{
-          alignItems: "center",
-          display: "flex",
-          flexGrow: 1,
-          minHeight: "100%",
-        }}
+    <div style={styles.paperContainer}>
+      <Button
+        sx={{ mt: "30%", ml: "5%" }}
+        component="a"
+        startIcon={<ArrowBackIcon fontSize="small" />}
+        variant="contained"
+        href="/"
       >
-        <Container maxWidth="md">
-          <Box
-            sx={{
-              alignItems: "center",
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <Typography align="center" color="textPrimary" variant="h1">
-              404: The page you are looking for isn’t here
-            </Typography>
-            <Typography align="center" color="textPrimary" variant="subtitle2">
-              You either tried some shady route or you came here by mistake.
-              Whichever it is, try using the navigation
-            </Typography>
-            <Box sx={{ textAlign: "center" }}>
-              <img
-                alt="Under development"
-                src="/static/images/undraw_page_not_found_su7k.svg"
-                style={{
-                  marginTop: 50,
-                  display: "inline-block",
-                  maxWidth: "100%",
-                  width: 560,
-                }}
-              />
-            </Box>
-            <a href="/" passHref>
-              <Button
-                component="a"
-                startIcon={<ArrowBackIcon fontSize="small" />}
-                sx={{ mt: 3 }}
-                variant="contained"
-              >
-                Go back to dashboard
-              </Button>
-            </a>
-          </Box>
-        </Container>
-      </Box>
+        Go back to Dashboard
+      </Button>
     </div>
   );
 }
