ef96a3c920c3f2121cfd3e30620bd1de87447375
Hoang Vinh <hoangvinh121299@gmail.com>
2021-12-21 03:35:42 +0700

    Revert "Merge branch 'fe/workplace' into develop"
    This reverts commit 7bc157d160b174eb2559b45e3f6efc3e3e985074.

--- frontend/src/components/MyDrawer.jsx
+++ frontend/src/components/MyDrawer.jsx
@@ -5,10 +5,8 @@ import Dashboard from "../pages/Dashboard";
 import Department from "../pages/Department";
 import Employee from "../pages/Employee";
 import Others from "../pages/Others";
-import AboutUs from "../pages/AboutUs";
+import AboutUs from "../pages/aboutUs";
 import NotFound from "../pages/404NotFound";
-import WorkPlace from "../pages/Workplace";
-
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -172,7 +170,7 @@ const pageList = [
   },
   {
     title: "Project",
-    path: "/workplace",
+    path: "/department",
     icon: <VscProject />,
     className: "nav-text",
   },
@@ -323,7 +321,6 @@ export default function AppBarComponent() {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="department" element={<Department />} />
           <Route path="employee" element={<Employee />} />
-          <Route path="workplace" element={<WorkPlace />} />
           <Route path="others" element={<Others />} />
           <Route path="about" element={<AboutUs />} />
           <Route path="*" element={<Navigate to="404" />} />
