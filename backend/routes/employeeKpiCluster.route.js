240e51651f3e57ef782adb7ae6a7150e90f718c1
Hoang Vinh <hoangvinh121299@gmail.com>
2022-01-16 23:18:45 +0700

    Update Kmean into Node

---
+++ backend/routes/employeeKpiCluster.route.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const { authJwt } = require("../middlewares");
+const verifySignUp = require("../middlewares/verifySignUp");
+const employeeKpiClustersControllers = require("../controllers/employeekpicluster.controller");
+
+// Router-level middleware
+router.get('/getCSV', employeeKpiClustersControllers.getEmployeeKpiClusterFromCSV)
+router.get("/get", employeeKpiClustersControllers.getEmployeeKpiCluster);
+router.post(
+  "/create",
+  employeeKpiClustersControllers.createEmployeeKpiCluster
+);
+
+router.post(
+  '/importCSV',
+  employeeKpiClustersControllers.importDataFromCSV
+);
+
+router.put("/update", employeeKpiClustersControllers.updateEmployeeKpiCluster);
+router.delete(
+  "/delete",
+  employeeKpiClustersControllers.deleteEmployeeKpiCluster
+);
+router.delete(
+  "/deleteall",
+  employeeKpiClustersControllers.deleteAllEmployeeKpiCluster
+);
+
+module.exports = router;
