d37b9c6e37bdaa1f9c0ddf229c88b72b915a8bcf
Hieu Pham <intelz.hieu@gmail.com>
2022-01-18 15:35:31 +0700

    d

--- backend/middlewares/dbConnection.js
+++ backend/middlewares/dbConnection.js
@@ -1,26 +1,26 @@
-require("dotenv").config({ path: "../config/.env" });
+// require("dotenv").config({ path: "../config/.env" });
 
-const mysql = require("mysql2");
+// const mysql = require("mysql2");
 
-const port = process.env.PORT || 8000;
-const mysql_host = process.env.MYSQL_HOST;
-const mysql_user = process.env.MYSQL_USER;
-const mysql_password = process.env.MYSQL_PASSWORD;
-const mysql_database = process.env.MYSQL_DATABASE;
+// const port = process.env.PORT || 8000;
+// const mysql_host = process.env.MYSQL_HOST;
+// const mysql_user = process.env.MYSQL_USER;
+// const mysql_password = process.env.MYSQL_PASSWORD;
+// const mysql_database = process.env.MYSQL_DATABASE;
 
-var con = mysql.createConnection({
-  host: mysql_host,
-  user: mysql_user,
-  password: mysql_password,
-  database: mysql_database,
-  timeout: 60000,
-});
+// var con = mysql.createConnection({
+//   host: mysql_host,
+//   user: mysql_user,
+//   password: mysql_password,
+//   database: mysql_database,
+//   timeout: 60000,
+// });
 
-con.connect((error) => {
-  if (error) {
-    throw error;
-  } else {
-    console.log("Connect to mysql server on ", mysql_host);
-  }
-});
-module.exports = con;
+// con.connect((error) => {
+//   if (error) {
+//     throw error;
+//   } else {
+//     console.log("Connect to mysql server on ", mysql_host);
+//   }
+// });
+// module.exports = con;
